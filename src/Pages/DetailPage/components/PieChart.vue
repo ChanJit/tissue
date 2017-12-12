cedbf677a64e251fde648bb5ff2ce0eab6ca513f
Zheng Yu Tay <tayzhengyu@seekasia.com>
2017-12-12 22:08:06 +0800

    - pie chart

--- src/Pages/DetailPage/components/PieChart.vue
+++ src/Pages/DetailPage/components/PieChart.vue
@@ -1,7 +1,6 @@
 <template>
   <chartjs-pie
     :option="{
-        legend: { display: false },
         scaleShowVerticalLines: false,
         scales: {
             xAxes: [{
@@ -48,6 +47,66 @@ export default {
     }
   }
 }
+function getBoxWidth(labelOpts, fontSize) {
+  return labelOpts.usePointStyle ?
+    fontSize * Math.SQRT2 :
+  labelOpts.boxWidth;
+};
+
+Chart.NewLegend = Chart.Legend.extend({
+  afterFit: function() {
+    this.height = this.height + 50;
+  },
+});
+
+function createNewLegendAndAttach(chartInstance, legendOpts) {
+  var legend = new Chart.NewLegend({
+    ctx: chartInstance.chart.ctx,
+    options: legendOpts,
+    chart: chartInstance
+  });
+  
+  if (chartInstance.legend) {
+    Chart.layoutService.removeBox(chartInstance, chartInstance.legend);
+    delete chartInstance.newLegend;
+  }
+  
+  chartInstance.newLegend = legend;
+  Chart.layoutService.addBox(chartInstance, legend);
+}
+
+// Register the legend plugin
+Chart.plugins.register({
+  beforeInit: function(chartInstance) {
+    var legendOpts = chartInstance.options.legend;
+
+    if (legendOpts) {
+      createNewLegendAndAttach(chartInstance, legendOpts);
+    }
+  },
+  beforeUpdate: function(chartInstance) {
+    var legendOpts = chartInstance.options.legend;
+
+    if (legendOpts) {
+      legendOpts = Chart.helpers.configMerge(Chart.defaults.global.legend, legendOpts);
+
+      if (chartInstance.newLegend) {
+        chartInstance.newLegend.options = legendOpts;
+      } else {
+        createNewLegendAndAttach(chartInstance, legendOpts);
+      }
+    } else {
+      Chart.layoutService.removeBox(chartInstance, chartInstance.newLegend);
+      delete chartInstance.newLegend;
+    }
+  },
+  afterEvent: function(chartInstance, e) {
+    var legend = chartInstance.newLegend;
+    if (legend) {
+      legend.handleEvent(e);
+    }
+  }
+});
 /*
 Example
 <Chart :data="[1,2,3,4,5]" :labels="['a','b','c','d','e']"/>
