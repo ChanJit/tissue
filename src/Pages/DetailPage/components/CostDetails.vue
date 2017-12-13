aae58f0b762e0f67f7684b5ff9f04e7420b144e9
Zheng Yu Tay <tayzhengyu@seekasia.com>
2017-12-13 20:13:34 +0800

    i dc

--- src/Pages/DetailPage/components/CostDetails.vue
+++ src/Pages/DetailPage/components/CostDetails.vue
@@ -2,12 +2,12 @@
   <v-flex row class="costDetailsContainer">
     <div class="containerTitle"> Estimated {{dateMode.name}} Cost </div>
     <div class="subtitle">below are rough estimation of your {{dateMode.name.toLowerCase()}} expenses on average, it would be varied for everyone</div>
-    <h2 class="totalExpense">RM {{totalExpenses}}</h2>
+    <h2 class="totalExpense">{{currency}} {{totalExpenses}}</h2>
     <template v-for="item in items" >
       <div v-bind:key="item.title">
         <v-layout class="center">
           <v-flex class="text-xs-left item-text" xs6 v-text="item.title"></v-flex>
-          <v-flex class="item-text">RM {{item.price}}</v-flex>
+          <v-flex class="item-text">{{currency}} {{item.price?item.price.toFixed(2):'0.00'}}</v-flex>
         </v-layout>
       </div>
     </template>
@@ -15,43 +15,47 @@
 </template>
 
 <script>
-  export default {
-    name: "cost-details",
-    props: {
-      items: {
-        type: Array
-      },
-      dateMode: {
-        type: Object
-      }
+export default {
+  name: 'cost-details',
+  props: {
+    currency: {
+      type: String
     },
-    computed: {
-      totalExpenses: function () {
-        const result = this.items.reduce((previous, current) => {
-            return {price: current.price + previous.price};
-        });
-
-        return result.price || 0;
-      }
+    items: {
+      type: Array
     },
-    mounted: function() {
-      console.log(this.items)
+    dateMode: {
+      type: Object
     }
+  },
+  computed: {
+    totalExpenses: function() {
+      if (this.items.length > 0) {
+        const result = this.items.reduce((previous, current) => {
+          return {price: current.price + previous.price}
+        })
+        return result.price.toFixed(2);
+      }
+      return 0
+    }
+  },
+  mounted: function() {
   }
+}
 </script>
 
 <style scoped>
-.center{
+.center {
   align-items: center;
   justify-content: center;
 }
 
-.costDetailsContainer{
+.costDetailsContainer {
   justify-content: center;
   align-items: center;
 }
 
-.containerTitle{
+.containerTitle {
   color: #2d3092;
   font-size: 24px;
   font-weight: bold;
@@ -59,18 +63,18 @@
   text-align: center;
 }
 
-.totalExpense{
+.totalExpense {
   margin-top: 10px;
   margin-bottom: 40px;
 }
 
-.subtitle{
+.subtitle {
   font-style: italic;
   font-size: 12px;
   color: grey;
 }
 
-.item-text{
+.item-text {
   font-size: 15px;
   max-width: 200px;
 }
